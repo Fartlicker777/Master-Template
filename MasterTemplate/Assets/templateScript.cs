d69f5f417041579245fae0576c8fe36f8b924f56
Fartlicker777 <62307787+Fartlicker777@users.noreply.github.com>
2020-08-14 18:55:20 -0500

    Added template for tp support

--- MasterTemplate/Assets/templateScript.cs
+++ MasterTemplate/Assets/templateScript.cs
@@ -37,4 +37,12 @@ public class Template : MonoBehaviour {
     void Update () {
 
     }
+
+    #pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+    #pragma warning restore 414
+
+    IEnumerator ProcessTwitchCommand (string Command) {
+      yield return null;
+    }
 }
